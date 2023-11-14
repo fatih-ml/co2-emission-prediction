8881670178e9b03a40d61162c3e72c8425483254
fatsibumi <fatsibumi@gmail.com>
2023-11-14 15:22:39 +0300

    model built

---
+++ functions.py
@@ -0,0 +1,22 @@
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
+
+def train_val(y_train, y_train_pred, y_test, y_pred, model_name):
+    
+    scores = {
+        f'{model_name}_train': {
+            "R2" : r2_score(y_train, y_train_pred),
+            "mae" : mean_absolute_error(y_train, y_train_pred),
+            "mse" : mean_squared_error(y_train, y_train_pred),
+            "rmse" : np.sqrt(mean_squared_error(y_train, y_train_pred))
+        },
+        f'{model_name}_test':  {
+            "R2" : r2_score(y_test, y_pred),
+            "mae" : mean_absolute_error(y_test, y_pred),
+            "mse" : mean_squared_error(y_test, y_pred),
+            "rmse" : np.sqrt(mean_squared_error(y_test, y_pred))
+        }
+    }
+    
+    return pd.DataFrame(scores)
